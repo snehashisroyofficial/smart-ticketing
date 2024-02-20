f577be0afa3c7042c1ba287786c2fad487061723
snehashisroyofficial <snehashisroyofficial@gmail.com>
2024-02-20 22:14:33 +0530

    seat left complete

--- js/script.js
+++ js/script.js
@@ -1,20 +1,32 @@
 
 
 let count = 0;
+let seatLeft = document.getElementById('seat-left').innerText;
+
+
 
 const allBtn = document.getElementsByClassName("all-btn");
 for (const btn of allBtn) {
     btn.addEventListener('click', function (event) {
+
+
+
         event.target.classList.add("bg-green-500", "text-white");
         count += 1
         setInnetText("cart-count", count);
+
+
+        seatLeft = seatLeft - 1;
+        setInnetText("seat-left", seatLeft);
+
+
 
         const ticketName = event.target.innerText;   //we get the button innertext value
 
         const ticketAdd = document.getElementById('ticket-add')    //we target where to add the ticket 
 
         const div = document.createElement("div");     // create div tag
-        div.setAttribute = 'disabled'
+
         div.classList.add("text-style-full", "flex", "justify-between", "pt-4")   // we add class names under div tag
 
 
@@ -42,6 +54,7 @@ for (const btn of allBtn) {
         // total Cost
 
         totalCost('total-price', h13.innerText);
+
     })
 
 }
@@ -60,52 +73,6 @@ function totalCost(id, value) {
     const sum = convertedTotalCost + parseInt(value)  // we sum 
     setInnetText(id, sum);
 };
-
-
-
-
-
-
-//LAST EDIT 10:08 AM 20-02-24
-
-
-const coupons = "NEW15, Couple 20"
-const newCoupons = coupons.split(",");
-
-
-document.getElementById('counpon-input').addEventListener('keyup', function (event) {
-
-    const text = event.target.value;
-    console.log(text);
-
-    if (text === newCoupons.includes("NEW15")) {
-        console.log("hello")
-
-    }
-    else if (text === newCoupons.includes("Couple 20")) {
-        alert("hi bro you type NEW15 code");
-    }
-
-    else {
-
-
-    }
-})
-
-
-
-
-
-
-
-
-
-function grandTotal() {
-
-
-
-
-}
 
 
 
