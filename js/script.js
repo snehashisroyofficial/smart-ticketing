1c435964fa57662f8afd27ff1e66ebcb48364b63
snehashisroyofficial <snehashisroyofficial@gmail.com>
2024-02-20 10:11:21 +0530

    coupon section js pending

--- js/script.js
+++ js/script.js
@@ -1,21 +1,3 @@
-// const allBtn = document.getElementsByClassName("all-btn");
-
-// let count = 0;
-// let seatLeft = document.getElementById("seat-left")
-
-// for (const btn of allBtn) {
-//     btn.addEventListener('click', function (event) {
-//         event.target.classList.add("bg-green-400")
-//         count = count + 1;
-//         setTextInner("cart-count", count);
-
-//     })
-// }
-
-
-// function setTextInner(id, value) {
-//     document.getElementById(id).innerText = value;
-// }
 
 
 let count = 0;
